a08811a217c8048e9dd89423f89efc87babe02c8
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2020-02-22 02:29:35 +0600

    feat: solve Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = min = (a) => Array.isArray(a) && a.length > 0 ? a.reduce((v, r) => r > v ? v : r, Infinity) : 0;
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = max = (a) => Array.isArray(a) && a.length > 0 ? a.reduce((v, r) => r < v ? v : r, -Infinity) : 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = avg = a => Array.isArray(a) && a.length > 0 ? a.reduce((v, s) => s += v, 0) / a.length : 0
